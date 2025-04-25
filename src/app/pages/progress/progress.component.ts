981cda71d9445d505747ea3dc35b4dd0110d8244
ddariod <dariod.dazaa09@gmail.com>
2025-04-25 10:29:35 -0500

    modify progressBar percentage by input and buttons

--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -3,9 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-progress',
   templateUrl: './progress.component.html',
-  styles: [
-  ]
+  styleUrls: ['./progress.component.css']
 })
 export class ProgressComponent {
+
+  progress: number = 15;
+
+  getPercentage() {
+    return `${this.progress}%`;
+  }
+
+  changePercentage(value: number) {
+    if (this.progress >= 100 && value >= 0) {
+      this.progress = 100;
+      return;
+    }
+
+    if (this.progress <= 0 && value < 0) {
+      this.progress = 0;
+      return;
+    }
+
+    this.progress = this.progress + value;
+  }
 
 }
