9450e6e6daaf2ea8b42d70f02f5748d08e6e7dca
ddariod <dariod.dazaa09@gmail.com>
2025-05-01 08:16:27 -0500

    move theme settings to a service

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SettingsService } from 'src/app/services/settings.service';
 
 @Component({
   selector: 'app-account-settings',
@@ -6,33 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styles: ``
 })
 export class AccountSettingsComponent implements OnInit {
-  public linkTheme = document.querySelector('#theme');
-  public links!: NodeListOf<Element>;
+  links!: NodeListOf<Element>;
+
+  constructor(private settingsServices: SettingsService) { }
 
   ngOnInit(): void {
     this.links = document.querySelectorAll('.selector');
-    this.checkCurrentTheme();
+    this.settingsServices.checkCurrentTheme(this.links);
   }
 
   changeTheme(theme: string): void {
-    const url = `./assets/css/colors/${theme}.css`;
-
-    this.linkTheme?.setAttribute('href', url);
-    localStorage.setItem('theme', url);
-    this.checkCurrentTheme();
-
-  }
-
-  checkCurrentTheme() {
-    this.links.forEach(elem => {
-      elem.classList.remove('working');
-      const btnTheme = elem.getAttribute('data-theme');
-      const btnThemeUrl = `./assets/css/colors/${btnTheme}.css`;
-      const currentTheme = this.linkTheme?.getAttribute('href');
-
-      if (btnThemeUrl === currentTheme) {
-        elem.classList.add('working');
-      }
-    })
+    this.settingsServices.changeTheme(theme);
+    this.settingsServices.checkCurrentTheme(this.links);
   }
 }
--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SettingsService } from '../services/settings.service';
 
 @Component({
   selector: 'app-pages',
@@ -8,11 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PagesComponent implements OnInit {
   year = new Date().getFullYear();
-  linkTheme = document.querySelector('#theme');
+
+  constructor(private settingsServices: SettingsService) { }
 
   ngOnInit() {
-    const url = localStorage.getItem('theme') || './assets/css/colors/default-dark.css';
-    this.linkTheme?.setAttribute('href', url);
   }
 
 }
