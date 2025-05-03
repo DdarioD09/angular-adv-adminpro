7adb237a9cb3378b963816175803e5eb6cd4c91c
ddariod <dariod.dazaa09@gmail.com>
2025-05-03 11:34:09 -0500

    improve bread-crumbs component using observables

--- src/app/shared/bread-crumbs/bread-crumbs.component.ts
+++ src/app/shared/bread-crumbs/bread-crumbs.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { ActivatedRoute, ActivationEnd, Data, Router } from '@angular/router';
+import { filter, map, Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-bread-crumbs',
@@ -6,6 +8,33 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class BreadCrumbsComponent {
+
+export class BreadCrumbsComponent implements OnDestroy {
+  title!: string;
+  titleSubs$!: Subscription;
+
+  constructor(private router: Router, activatedRoute: ActivatedRoute) {
+    // this is another way to check the data in the active route, but it is static
+    // console.log(activatedRoute.snapshot.children[0].data);
+
+    this.titleSubs$ = this.getDataRouting().subscribe(({ title }) => {
+      this.title = title;
+      document.title = `AdminPro - ${title}`
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.titleSubs$.unsubscribe();
+  }
+
+  getDataRouting(): Observable<Data> {
+    return this.router.events
+      .pipe(
+        // filter(event => event instanceof ActivationEnd),
+        filter((event): event is ActivationEnd => event instanceof ActivationEnd),
+        filter((event) => event.snapshot.firstChild === null),
+        map((event): Data => event.snapshot.data)
+      )
+  }
 
 }
